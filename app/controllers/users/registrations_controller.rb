8db33925a31cd217b92b62d06d381b4c1b981666
Deb <deb@Emilys-MacBook-Air-2.local>
2021-05-21 15:46:49 +0200

    New signups receive 3 credits

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    @session.update(count: 0)
+  end
 
   # GET /resource/edit
   # def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
 	root 'static_pages#home'
 	get 'converted/:id' 				=> 'static_pages#converted', as: "converted"
 	
 	resources :uploaded_files, only: [:create, :destroy]
 
 	mount PdfjsViewer::Rails::Engine 	=> '/pdfjs', as: "pdfjs"
+
+	devise_for :users, controllers: { registrations: 'users/registrations' }
 end
